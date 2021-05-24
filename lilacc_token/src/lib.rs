f28b3222c33887a9cecc42a0db27ebedb50dff5a
Taiki Harada <haradataiki19990927@gmail.com>
2021-05-24 11:19:11 +0000

    Implement Token and TokenKind

---
+++ lilacc_token/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub literal: String,
+}
+
+impl Token {
+    pub fn new<T: Into<String>>(kind: TokenKind, literal: T) -> Token {
+        Token {
+            kind: kind,
+            literal: literal.into(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum TokenKind {
+    Num,
+
+    Plus,
+    Minus,
+    Star,
+    Slash,
+
+    Ident,
+
+    Eof,
+}
