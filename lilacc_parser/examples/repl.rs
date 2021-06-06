1300383b785d7f2bad3a8f6dd9da5f4d62afd73a
Taiki Harada <haradataiki19990927@gmail.com>
2021-06-06 14:58:12 +0000

    Add parser example

---
+++ lilacc_parser/examples/repl.rs
@@ -0,0 +1,26 @@
+use lilacc_ast::*;
+use lilacc_lexer::Lexer;
+use lilacc_parser::lilac;
+use std::io::{stdout, Write};
+
+fn main() -> std::io::Result<()> {
+    loop {
+        print!(">> ");
+        let _ = stdout().flush();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+
+        match input.as_str().trim() {
+            "quit" | ":q" => break,
+            input => println!("{:#?}", parse(input)),
+        }
+    }
+
+    Ok(())
+}
+
+fn parse(input: &str) -> Expr {
+    let lexer = Lexer::new(input);
+    lilac::ExprParser::new().parse(lexer).unwrap()
+}
