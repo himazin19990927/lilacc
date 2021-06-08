fa4ecb333c0c837e247af3a6c2c8badb4a96f3d7
Taiki Harada <haradataiki19990927@gmail.com>
2021-06-08 09:28:43 +0000

    Add comment

--- lilacc_ast/src/token.rs
+++ lilacc_ast/src/token.rs
@@ -1,28 +1,63 @@
 use crate::lit::*;
 #[derive(Debug, PartialEq, Clone)]
 pub enum Token {
+    /// A literal token: `1`, `true`
     Lit(Lit),
 
+    /// An identity token: `x`, `foo`
     Ident(String),
 
+    /// The `=` token
     Eq,
+
+    /// The `<` token
     Lt,
+
+    /// The `<=` token
     Le,
+
+    /// The `==` token
     EqEq,
+
+    /// The `!=` token
     Ne,
+
+    /// The `>=` token
     Ge,
+
+    /// The `>` token
     Gt,
+
+    /// The `&&` token
     AndAnd,
+
+    /// The `||` token
     OrOr,
+
+    /// The `!` token
     Not,
 
+    /// The `+` token
     Plus,
+
+    /// The `-` token
     Minus,
+
+    /// The `*` token
     Star,
+
+    /// The `/` token
     Slash,
+
+    /// The `&` token
     And,
+
+    /// The `|` token
     Or,
 
+    /// The `(` token
     OpenParen,
+
+    /// The `)` token
     CloseParen,
 }
