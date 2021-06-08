4b8cb2886959ac8c1d64cf9e7ec27a4951b4f934
Taiki Harada <haradataiki19990927@gmail.com>
2021-06-08 09:23:04 +0000

    Add comments

--- lilacc_ast/src/lib.rs
+++ lilacc_ast/src/lib.rs
@@ -5,33 +5,57 @@ use lit::Lit;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum BinOp {
+    /// The `*` operator (multiplication)
     Mul,
+
+    /// The `/` operator (division)
     Div,
 
+    /// The `+` operator (addition)
     Add,
+
+    /// The `-` operator (subtraction)
     Sub,
 
+    /// The `<` operator (less than)
     Lt,
+
+    /// The `<=` operator (less than or equal to)
     Le,
 
+    /// The `==` operator (equal to)
     Eq,
+
+    /// The `!=` operator (not equal to)
     Ne,
 
+
+    /// The `&&` operator (logical and)
     And,
+
+    /// The `||` operator (logical or)
     Or,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum UnOp {
+    /// The `-` operator (negation)
     Neg,
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expr {
+    /// A binary operation: `a + b`, "a && b"
     Binary(ExprBinary),
+
+    /// A unary operation: `-x`
     Unary(ExprUnary),
 
+
+    /// A literal in place of an expression: `1`, `true`
     Lit(ExprLit),
+
+    /// An identity like `x`, `foo`
     Ident(ExprIdent),
 }
 
