42eb966439a26802a9f8604c824473b17d315e44
Taiki Harada <haradataiki19990927@gmail.com>
2021-06-09 05:10:10 +0000

    Add test

--- lilacc_parser/tests/stmt.rs
+++ lilacc_parser/tests/stmt.rs
@@ -1,4 +1,4 @@
-use lilacc_ast::{expr::*, lit::*, op::*, pat::*, stmt::*, *};
+use lilacc_ast::{expr::*, lit::*, op::*, pat::*, stmt::*, ty::*, *};
 use lilacc_lexer::Lexer;
 use lilacc_parser::*;
 
@@ -12,7 +12,7 @@ macro_rules! test_stmt {
 }
 
 #[test]
-fn parse_stmt() {
+fn parse_local() {
     test_stmt!(
         "let x = 0;",
         Stmt::Local(Local {
@@ -23,11 +23,36 @@ fn parse_stmt() {
         })
     );
 
+    let pt = PatType {
+        pat: Box::new(Pat::Ident(PatIdent {
+            ident: "x".to_string(),
+        })),
+        ty: Box::new(Type::Ident(TypeIdent {
+            name: "i32".to_string(),
+        })),
+    };
+
+    let pat = Pat::Type(pt);
+
+    test_stmt!(
+        "let x: i32 = 0;",
+        Stmt::Local(Local {
+            pat: pat,
+            init: expr_int!(0)
+        })
+    );
+}
+
+#[test]
+fn parse_expr() {
     test_stmt!(
         "a+b",
         Stmt::Expr(expr_binary!(expr_ident!("a"), BinOp::Add, expr_ident!("b")))
     );
+}
 
+#[test]
+fn parse_semi() {
     test_stmt!(
         "a+b;",
         Stmt::Semi(expr_binary!(expr_ident!("a"), BinOp::Add, expr_ident!("b")))
