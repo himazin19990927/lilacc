1d90d28c0494746ce5018e0a776e855e63aa573e
Taiki Harada <haradataiki19990927@gmail.com>
2021-06-12 07:41:24 +0000

    Implement FnSignature

--- lilacc_ast/src/item.rs
+++ lilacc_ast/src/item.rs
@@ -1,5 +1,10 @@
 use crate::pat::*;
 
+pub struct FnSignature {
+    pub ident: String,
+    pub input: Vec<FnArg>,
+}
+
 pub enum FnArg {
     /// A function argument of type-annotated: `x: i32`, `foo: f64`
     Typed(PatType),
